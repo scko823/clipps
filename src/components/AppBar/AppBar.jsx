582acdbf704d231a293f7e4e2604893c8f7d25ba
Sin Cheung Ko <scko823@gmail.com>
2017-11-11 10:28:17 -0700

    prelim first queries

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,29 +1,38 @@
-import React from 'react';
-import MUIAppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
-import Button from 'material-ui/Button';
-import IconButton from 'material-ui/IconButton';
-import MenuIcon from 'material-ui-icons/Menu';
-import Drawer from 'material-ui/Drawer';
-import DrawerList from './DrawerList';
+import React from 'react'
+import MUIAppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import Typography from 'material-ui/Typography'
+import Button from 'material-ui/Button'
+import IconButton from 'material-ui/IconButton'
+import MenuIcon from 'material-ui-icons/Menu'
+import Drawer from 'material-ui/Drawer'
+
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+import allClipboardsQuery from '../../../graphql/queries/allClipboards'
+
+import DrawerList from './DrawerList'
 
 class ClipboardAppBar extends React.Component {
 	state = {
 		showDrawer: false,
-	};
+	}
 	toggleDrawer = () => {
 		this.setState(({ showDrawer }) => ({
 			showDrawer: !showDrawer,
-		}));
-	};
+		}))
+	}
 
 	render() {
 		return (
   <div id="clipboard">
     <MUIAppBar position="static">
       <Toolbar>
-        <IconButton onClick={this.toggleDrawer} color="contrast" aria-label="Menu">
+        <IconButton
+          onClick={this.toggleDrawer}
+          color="contrast"
+          aria-label="Menu"
+        >
           <MenuIcon />
         </IconButton>
         <Typography type="title" color="inherit">
@@ -40,8 +49,10 @@ class ClipboardAppBar extends React.Component {
       <DrawerList />
     </Drawer>
   </div>
-		);
+		)
 	}
 }
 
-export default ClipboardAppBar;
+const withAllClipboardsQuery = graphql(gql`${allClipboardsQuery}`)
+
+export default withAllClipboardsQuery(ClipboardAppBar)
--- src/components/AppBar/DrawerList.jsx
+++ src/components/AppBar/DrawerList.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Divider from 'material-ui/Divider';
-import ListSubheader from 'material-ui/List/ListSubheader';
-import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
-import { withStyles } from 'material-ui/styles';
+import React from 'react'
+import PropTypes from 'prop-types'
+import Divider from 'material-ui/Divider'
+import ListSubheader from 'material-ui/List/ListSubheader'
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List'
+import { withStyles } from 'material-ui/styles'
 
 const styles = theme => ({
 	root: {
@@ -15,16 +15,19 @@ const styles = theme => ({
 	nested: {
 		paddingLeft: theme.spacing.unit * 4,
 	},
-});
+})
 
 const DrawerList = ({ clips, classes }) => (
-  <List className={classes.root} subheader={<ListSubheader>Clipboards</ListSubheader>} />
-);
+  <List
+    className={classes.root}
+    subheader={<ListSubheader>Clipboards</ListSubheader>}
+  />
+)
 
 DrawerList.propTypes = {
 	clips: PropTypes.array,
 	classes: PropTypes.object,
-};
+}
 
 DrawerList.defaultProps = {
 	clips: [],
@@ -36,5 +39,6 @@ DrawerList.defaultProps = {
 			background: 'white',
 		},
 	},
-};
-export default withStyles(styles)(DrawerList);
+}
+
+export default withStyles(styles)(DrawerList)
