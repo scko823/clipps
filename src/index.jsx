7fe4146351c77acb75d3b55228e19e56da71b65f
Sin Cheung Ko <scko823@gmail.com>
2017-11-10 19:39:02 -0700

    add prelim config for flow

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,27 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Route } from 'react-router';
-import { Provider } from 'react-redux';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+// @flow weak
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Route } from 'react-router'
+import { Provider } from 'react-redux'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles'
 
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
-import AppBar from './components/AppBar/AppBar';
-import App from './components/App';
-import Landing from './components/Landing';
+import AppBar from './components/AppBar/AppBar'
+import App from './components/App'
+import Landing from './components/Landing'
 
-import store from './stores/Store';
-import './styles/main.scss';
+import store from './stores/Store'
 // import Clipboard from './components/Clipboard'
 
 const client = new ApolloClient({
 	link: new HttpLink({ uri: 'https://api.example.com/graphql' }),
 	cache: new InMemoryCache(),
-});
+})
 
 const theme = createMuiTheme({
 	palette: {
@@ -30,7 +30,7 @@ const theme = createMuiTheme({
 	appBar: {
 		height: 50,
 	},
-});
+})
 
 ReactDOM.render(
   <ApolloProvider client={client}>
@@ -45,8 +45,8 @@ ReactDOM.render(
     </Provider>
   </ApolloProvider>,
 	document.getElementById('root'), // eslint-disable-line
-);
+)
 
 if (module.hot) {
-	module.hot.accept();
+	module.hot.accept()
 }
