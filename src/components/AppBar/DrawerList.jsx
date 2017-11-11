67abc4bc4dd4abf039372682a3ceca8cec05ba57
Sin Cheung Ko <scko823@gmail.com>
2017-11-11 11:20:15 -0700

    displaying item list

--- src/components/AppBar/DrawerList.jsx
+++ src/components/AppBar/DrawerList.jsx
@@ -1,47 +1,61 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Divider from 'material-ui/Divider'
-import ListSubheader from 'material-ui/List/ListSubheader'
-import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List'
-import { withStyles } from 'material-ui/styles'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Divider from 'material-ui/Divider';
+import ListSubheader from 'material-ui/List/ListSubheader';
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+import { withStyles } from 'material-ui/styles';
+import DataUsageIcon from 'material-ui-icons/DataUsage';
 
 const styles = theme => ({
-	root: {
-		width: '100%',
-		maxWidth: 360,
-		minWidth: 200,
-		background: theme.palette.background.paper,
-	},
-	nested: {
-		paddingLeft: theme.spacing.unit * 4,
-	},
-})
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    minWidth: 200,
+    background: theme.palette.background.paper,
+  },
+  subheader: {
+    fontWeight: 'bold',
+  },
+  nested: {
+    paddingLeft: theme.spacing.unit * 4,
+  },
+});
 
-const DrawerList = ({ loading, clipboards, refetch, classes }) => (
+const DrawerList = ({
+  loading, clipboards, refetch, classes,
+}) => (
   <List
     className={classes.root}
-    subheader={<ListSubheader>Clipboards</ListSubheader>}
+    subheader={<ListSubheader className="whatever">Clipboards</ListSubheader>}
   >
     <Divider />
-    {loading ? <h6>loading</h6> : JSON.stringify(clipboards)}
+    {loading ? (
+      <DataUsageIcon />
+		) : (
+			clipboards.map(clipboard => (
+  <ListItem key={clipboard.id} button>
+    <ListItemText primary={clipboard.name} />
+  </ListItem>
+			))
+		)}
   </List>
-)
+);
 
 DrawerList.propTypes = {
-	clipboards: PropTypes.arrayOf(PropTypes.object),
-	classes: PropTypes.object,
-}
+  clipboards: PropTypes.arrayOf(PropTypes.object),
+  classes: PropTypes.object,
+};
 
 DrawerList.defaultProps = {
-	clipboards: [],
-	classes: {
-		root: {
-			width: '100%',
-			maxWidth: 360,
-			minWidth: 200,
-			background: 'white',
-		},
-	},
-}
+  clipboards: [],
+  classes: {
+    root: {
+      width: '100%',
+      maxWidth: 360,
+      minWidth: 200,
+      background: 'white',
+    },
+  },
+};
 
-export default withStyles(styles)(DrawerList)
+export default withStyles(styles)(DrawerList);
