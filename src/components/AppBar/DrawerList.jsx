840c8bb592c0af7e0781e3c54ab3a11266cf7d68
Sin Cheung Ko <scko823@gmail.com>
2017-11-11 11:26:23 -0700

    refactor DrawerList

--- src/components/AppBar/DrawerList.jsx
+++ src/components/AppBar/DrawerList.jsx
@@ -1,61 +1,63 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Divider from 'material-ui/Divider';
-import ListSubheader from 'material-ui/List/ListSubheader';
-import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
-import { withStyles } from 'material-ui/styles';
-import DataUsageIcon from 'material-ui-icons/DataUsage';
+import React from 'react'
+import PropTypes from 'prop-types'
+import Divider from 'material-ui/Divider'
+import ListSubheader from 'material-ui/List/ListSubheader'
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List'
+import { withStyles } from 'material-ui/styles'
+import DataUsageIcon from 'material-ui-icons/DataUsage'
 
 const styles = theme => ({
-  root: {
-    width: '100%',
-    maxWidth: 360,
-    minWidth: 200,
-    background: theme.palette.background.paper,
-  },
-  subheader: {
-    fontWeight: 'bold',
-  },
-  nested: {
-    paddingLeft: theme.spacing.unit * 4,
-  },
-});
+	root: {
+		width: '100%',
+		maxWidth: 360,
+		minWidth: 200,
+		background: theme.palette.background.paper,
+	},
+	subheader: {
+		fontWeight: 'bold',
+	},
+	nested: {
+		paddingLeft: theme.spacing.unit * 4,
+	},
+})
 
-const DrawerList = ({
-  loading, clipboards, refetch, classes,
-}) => (
-  <List
-    className={classes.root}
-    subheader={<ListSubheader className="whatever">Clipboards</ListSubheader>}
-  >
-    <Divider />
-    {loading ? (
-      <DataUsageIcon />
-		) : (
-			clipboards.map(clipboard => (
+const DrawerList = ({ loading, clipboards, refetch, classes }) => {
+	const subheader = (
+  <ListSubheader className="whatever">Clipboards</ListSubheader>
+	)
+	let listItems
+	if (loading) {
+		listItems = <DataUsageIcon />
+	} else if (clipboards) {
+		listItems = clipboards.map(clipboard => (
   <ListItem key={clipboard.id} button>
     <ListItemText primary={clipboard.name} />
   </ListItem>
-			))
-		)}
+		))
+	}
+	return (
+  <List className={classes.root} subheader={subheader}>
+    <Divider />
+    {listItems}
   </List>
-);
+	)
+}
 
 DrawerList.propTypes = {
-  clipboards: PropTypes.arrayOf(PropTypes.object),
-  classes: PropTypes.object,
-};
+	clipboards: PropTypes.arrayOf(PropTypes.object),
+	classes: PropTypes.object,
+}
 
 DrawerList.defaultProps = {
-  clipboards: [],
-  classes: {
-    root: {
-      width: '100%',
-      maxWidth: 360,
-      minWidth: 200,
-      background: 'white',
-    },
-  },
-};
+	clipboards: [],
+	classes: {
+		root: {
+			width: '100%',
+			maxWidth: 360,
+			minWidth: 200,
+			background: 'white',
+		},
+	},
+}
 
-export default withStyles(styles)(DrawerList);
+export default withStyles(styles)(DrawerList)
