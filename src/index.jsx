28d3a055a9b8284caf80dd1aee5aeac98edc74bc
Sin Ko <sko@axway.com>
2017-11-12 10:10:04 -0700

    fix hot replacement

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,19 @@
-import { Provider } from 'react-redux'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles'
+import { Provider } from 'react-redux';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 
-import { ApolloProvider } from 'react-apollo'
-import { split } from 'apollo-link'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { ApolloClient } from 'apollo-client'
-import { WebSocketLink } from 'apollo-link-ws'
-import { HttpLink } from 'apollo-link-http'
-import { getMainDefinition } from 'apollo-utilities'
+import { ApolloProvider } from 'react-apollo';
+import { split } from 'apollo-link';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
+import { WebSocketLink } from 'apollo-link-ws';
+import { HttpLink } from 'apollo-link-http';
+import { getMainDefinition } from 'apollo-utilities';
 
-import AppBar from './components/AppBar/AppBar'
-// import App from './components/App'
+import AppBar from './components/AppBar/AppBar';
 
-import store from './stores/Store'
-// import Clipboard from './components/Clipboard'
+import store from './stores/Store';
 
 const theme = createMuiTheme({
 	palette: {
@@ -24,35 +22,36 @@ const theme = createMuiTheme({
 	appBar: {
 		height: 50,
 	},
-})
+});
 
 const httpLink = new HttpLink({
 	uri: process.env.QUERY_API,
-})
+});
 
 const wsLink = new WebSocketLink({
 	uri: process.env.SUBSCRIPTION_API,
 	options: {
 		reconnect: true,
 	},
-})
+});
 
 const link = split(
 	// split based on operation type
 	({ query }) => {
-		const { kind, operation } = getMainDefinition(query)
-		return kind === 'OperationDefinition' && operation === 'subscription'
+		const { kind, operation } = getMainDefinition(query);
+		return kind === 'OperationDefinition' && operation === 'subscription';
 	},
 	wsLink,
 	httpLink,
-)
+);
 
 const client = new ApolloClient({
 	link,
 	cache: new InMemoryCache(),
-})
+});
 
-ReactDOM.render(
+const render = () => {
+	ReactDOM.render(
   <ApolloProvider client={client}>
     <Provider store={store}>
       <MuiThemeProvider theme={theme}>
@@ -60,9 +59,12 @@ ReactDOM.render(
       </MuiThemeProvider>
     </Provider>
   </ApolloProvider>,
-	document.getElementById('root'), // eslint-disable-line
-)
+		document.getElementById('root'), // eslint-disable-line
+	);
+};
+
+render();
 
 if (module.hot) {
-	module.hot.accept()
+	module.hot.accept('./components/AppBar/AppBar', render);
 }
