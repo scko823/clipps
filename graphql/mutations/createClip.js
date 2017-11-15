4c037341cb68bab212f1b5e8d0a3f4b13d75ac8d
Sin Cheung Ko <scko823@gmail.com>
2017-11-14 20:29:07 -0700

    add ID as required field for query

--- graphql/mutations/createClip.js
+++ graphql/mutations/createClip.js
@@ -1,6 +1,6 @@
 // crete a new clip in a particular clipboard
 export default `
-mutation createClip($content: String!, $name: String!, $clipboardId: ID) {
+mutation createClip($content: String!, $name: String!, $clipboardId: ID!) {
   createClip(content: $content, name: $name, clipboardId: $clipboardId) {
     id
   }
