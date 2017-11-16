3ff0a0600318239cf5694989357dd0f93528d6b1
Sin Cheung Ko <scko823@gmail.com>
2017-11-15 20:49:14 -0700

    remove redux store

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,16 @@
-import { Provider } from 'react-redux';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles'
 
-import { ApolloProvider } from 'react-apollo';
-import { split } from 'apollo-link';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloClient } from 'apollo-client';
-import { WebSocketLink } from 'apollo-link-ws';
-import { HttpLink } from 'apollo-link-http';
-import { getMainDefinition } from 'apollo-utilities';
+import { ApolloProvider } from 'react-apollo'
+import { split } from 'apollo-link'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloClient } from 'apollo-client'
+import { WebSocketLink } from 'apollo-link-ws'
+import { HttpLink } from 'apollo-link-http'
+import { getMainDefinition } from 'apollo-utilities'
 
-import AppBar from './components/AppBar/AppBar';
-
-import store from './stores/Store';
+import AppBar from './components/AppBar/AppBar'
 
 const theme = createMuiTheme({
 	palette: {
@@ -22,49 +19,47 @@ const theme = createMuiTheme({
 	appBar: {
 		height: 50,
 	},
-});
+})
 
 const httpLink = new HttpLink({
 	uri: process.env.QUERY_API,
-});
+})
 
 const wsLink = new WebSocketLink({
 	uri: process.env.SUBSCRIPTION_API,
 	options: {
 		reconnect: true,
 	},
-});
+})
 
 const link = split(
 	// split based on operation type
 	({ query }) => {
-		const { kind, operation } = getMainDefinition(query);
-		return kind === 'OperationDefinition' && operation === 'subscription';
+		const { kind, operation } = getMainDefinition(query)
+		return kind === 'OperationDefinition' && operation === 'subscription'
 	},
 	wsLink,
 	httpLink,
-);
+)
 
 const client = new ApolloClient({
 	link,
 	cache: new InMemoryCache(),
-});
+})
 
 const render = () => {
 	ReactDOM.render(
   <ApolloProvider client={client}>
-    <Provider store={store}>
-      <MuiThemeProvider theme={theme}>
-        <AppBar />
-      </MuiThemeProvider>
-    </Provider>
+    <MuiThemeProvider theme={theme}>
+      <AppBar />
+    </MuiThemeProvider>
   </ApolloProvider>,
 		document.getElementById('root'), // eslint-disable-line
-	);
-};
+	)
+}
 
-render();
+render()
 
 if (module.hot) {
-	module.hot.accept('./components/AppBar/AppBar', render);
+	module.hot.accept('./components/AppBar/AppBar', render)
 }
