b97a4b0e9d201da50e0cb3268eb685e07fbdbb22
Sin Cheung Ko <scko823@gmail.com>
2017-11-18 17:15:48 -0700

    now minify setting

--- webpack.config.prod.babel.js
+++ webpack.config.prod.babel.js
@@ -1,24 +1,21 @@
 import merge from 'webpack-merge'
-import MinifyPlugin from 'babel-minify-webpack-plugin'
+// import MinifyPlugin from 'babel-minify-webpack-plugin'
+
+import UglifyJsPlugin from 'uglifyjs-webpack-plugin'
+
 import common from './webpack.config.common.babel'
 
 const prodConfig = merge(common, {
 	plugins: [
-		new MinifyPlugin({
-			removeConsole: true,
-			removeDebugger: true,
+		// new MinifyPlugin({
+		// 	removeConsole: true,
+		// 	removeDebugger: true,
+		// }),
+		new UglifyJsPlugin({
+			cache: true,
 		}),
 	],
 
 	devtool: 'source-map',
 })
 export default prodConfig
-
-// entry: ['babel-polyfill', path.join(__dirname, 'src', 'index.jsx')],
-// output: {
-// 	filename: '[name].[hash].bundle.js',
-// 	path: path.resolve(__dirname, 'dist'),
-// },
-// resolve: {
-// 	extensions: ['.js', '.json', '.jsx'],
-// },
