59d03a0032c513b59a72238b820a7efafd33ff28
Sin Ko <sko@axway.com>
2017-11-19 17:51:01 -0700

    flow setting

--- flow-typed/material-ui_vx.x.x.js
+++ flow-typed/material-ui_vx.x.x.js
@@ -1,15 +1,36 @@
 declare module 'material-ui' {
-	declare module.exports: any
+	declare module.exports: any;
 }
 
+declare module 'material-ui/Button' {
+	declare module.exports: any;
+}
+
+declare module 'material-ui/colors' {
+	declare module.exports: any;
+}
 declare module 'material-ui/Divider' {
-	declare module.exports: any
+	declare module.exports: any;
+}
+declare module 'material-ui/Form/FormGroup' {
+	declare module.exports: any;
+}
+
+declare module 'material-ui/Grid' {
+	declare module.exports: any;
 }
 
 declare module 'material-ui/List' {
-	declare module.exports: any
+	declare module.exports: any;
 }
 
 declare module 'material-ui/List/ListSubheader' {
-	declare module.exports: any
+	declare module.exports: any;
+}
+
+declare module 'material-ui/styles/withStyles' {
+	declare module.exports: any;
+}
+declare module 'material-ui/TextField' {
+	declare module.exports: any;
 }
