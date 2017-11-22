1ea4988c0a2d4b427db33e196798e4988d8f43ef
Sin Cheung Ko <scko823@gmail.com>
2017-11-21 22:17:01 -0700

    add FAB flow type

--- flow-typed/component-types.js
+++ flow-typed/component-types.js
@@ -11,3 +11,10 @@ type SnippetPropType = {
 	copyContent: (e: SyntheticMouseEvent<*>) => void,
 	clipboardName: string,
 };
+
+type FABPropType = {
+	classes: {
+		fab: cssInJS,
+	},
+	onClick: (e: SyntheticMouseEvent<*>) => void,
+};
