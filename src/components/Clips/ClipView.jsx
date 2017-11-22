ddabdd9be3f8249137d3e93ddcf7a5f2f52dd15d
Sin Cheung Ko <scko823@gmail.com>
2017-11-21 22:18:43 -0700

    add flow for ClipView

--- src/components/Clips/ClipView.jsx
+++ src/components/Clips/ClipView.jsx
@@ -1,13 +1,13 @@
-// import React from 'react'
-import { withStyles } from 'material-ui/styles'
-import { compose } from 'recompose'
+// @flow
+import { withStyles } from 'material-ui/styles';
+import { compose } from 'recompose';
 
 // GraphQL
-import { graphql } from 'react-apollo'
-import gql from 'graphql-tag'
-import clipByBoardAndClipNameQuery from '../../../graphql/queries/clipByBoardAndClipName'
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import clipByBoardAndClipNameQuery from '../../../graphql/queries/clipByBoardAndClipName';
 
-import { withCopyEnhancer, Snippet, snippetStyles } from './Snippet'
+import { withCopyEnhancer, Snippet, snippetStyles } from './Snippet';
 
 const clipViewStyles = () => ({
 	...snippetStyles,
@@ -16,26 +16,19 @@ const clipViewStyles = () => ({
 		width: '100%',
 		margin: '0',
 	},
-})
+});
 
 const withClipQuery = graphql(gql`${clipByBoardAndClipNameQuery}`, {
 	options: ({ match: { params: { clipboardName, clipName } } }) => ({
 		variables: { clipboardName, clipName },
 	}),
-	props: ({
-		ownProps,
-		data: { loading, allClips = [{ name: '', content: '' }], refetch },
-	}) => ({
+	props: ({ ownProps, data: { loading, allClips = [{ name: '', content: '' }], refetch } }) => ({
 		...ownProps,
 		clipboardName: ownProps.match.params.clipboardName,
 		clip: allClips[0],
 		loading,
 		refetch,
 	}),
-})
+});
 
-export default compose(
-	withStyles(clipViewStyles),
-	withCopyEnhancer,
-	withClipQuery,
-)(Snippet)
+export default compose(withStyles(clipViewStyles), withCopyEnhancer, withClipQuery)(Snippet);
