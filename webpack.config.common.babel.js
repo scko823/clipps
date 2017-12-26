9be8f2e7debf5691d7722ebd492e20b9b080e399
Sin Cheung Ko <scko823@gmail.com>
2017-12-26 13:21:20 -0700

    init fix NOW board logic to create at app lanuch

--- webpack.config.common.babel.js
+++ webpack.config.common.babel.js
@@ -3,62 +3,62 @@ import path from 'path';
 import HtmkWebpackPlugin from 'html-webpack-plugin';
 import endpoints from './endpoints.json';
 
-const { graphcool: { websockets, api } } = endpoints;
-
+const envEndpoints = process.env.NODE_ENV === 'production' ? endpoints.dev : endpoints.graphcool;
+const { websockets, api } = envEndpoints;
 const htmlPluginDefaultOpts = {
-	template: path.join(__dirname, 'src', 'index.ejs'),
-	inject: true,
+    template: path.join(__dirname, 'src', 'index.ejs'),
+    inject: true
 };
 export default {
-	entry: {
-		app: ['babel-polyfill', path.join(__dirname, 'src', 'index.jsx')],
-	},
-	output: {
-		filename: '[name].[hash].bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-		publicPath: '/',
-	},
-	resolve: {
-		extensions: ['.js', '.json', '.jsx'],
-	},
-	module: {
-		rules: [
-			// JS Loader
-			{
-				test: /\.jsx?$/,
-				use: {
-					loader: 'babel-loader',
-				},
-				exclude: '/node_modules/',
-				include: [path.join(__dirname, 'src')],
-			},
-			// Images
-			{
-				test: /\.(png|jpg|gif|woff|woff2|ttf|eot|svg)/,
-				use: {
-					loader: 'file-loader',
-					options: {
-						limit: 8192,
-					},
-				},
-			},
-		],
-	},
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
-				QUERY_API: JSON.stringify(api),
-				SUBSCRIPTION_API: JSON.stringify(websockets),
-			},
-		}),
-		new webpack.NoEmitOnErrorsPlugin(),
-		new HtmkWebpackPlugin({ ...htmlPluginDefaultOpts }),
-		new HtmkWebpackPlugin({
-			...htmlPluginDefaultOpts,
-			filename: '200.html',
-		}),
+    entry: {
+        app: ['babel-polyfill', path.join(__dirname, 'src', 'index.jsx')]
+    },
+    output: {
+        filename: '[name].[hash].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    resolve: {
+        extensions: ['.js', '.json', '.jsx']
+    },
+    module: {
+        rules: [
+            // JS Loader
+            {
+                test: /\.jsx?$/,
+                use: {
+                    loader: 'babel-loader'
+                },
+                exclude: '/node_modules/',
+                include: [path.join(__dirname, 'src')]
+            },
+            // Images
+            {
+                test: /\.(png|jpg|gif|woff|woff2|ttf|eot|svg)/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        limit: 8192
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+                QUERY_API: JSON.stringify(api),
+                SUBSCRIPTION_API: JSON.stringify(websockets)
+            }
+        }),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new HtmkWebpackPlugin({ ...htmlPluginDefaultOpts }),
+        new HtmkWebpackPlugin({
+            ...htmlPluginDefaultOpts,
+            filename: '200.html'
+        }),
 
-		new webpack.NamedModulesPlugin(),
-	],
+        new webpack.NamedModulesPlugin()
+    ]
 };
