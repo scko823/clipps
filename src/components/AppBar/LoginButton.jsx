0d70ff407c8641a7016736e74fe7e59ef5543db5
Sin Cheung Ko <scko823@gmail.com>
2018-01-02 20:39:34 -0700

    prelim add login button toggle logic

--- src/components/AppBar/LoginButton.jsx
+++ src/components/AppBar/LoginButton.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 // import { withRouter } from 'react-router';
 // import Button from 'material-ui/Button';
+import Popover from 'material-ui/Popover';
 import IconButton from 'material-ui/IconButton';
 import AccountCircle from 'material-ui-icons/AccountCircle';
 // import { grey, blue, lightGreen } from 'material-ui/colors';
@@ -13,27 +14,54 @@ import AccountCircle from 'material-ui-icons/AccountCircle';
 import { withStyles } from 'material-ui/styles';
 // import SendIcon from 'material-ui-icons/Send';
 
-// import { compose, withStateHandlers, withProps } from 'recompose';
+import { compose, withStateHandlers } from 'recompose';
 
 const styles = () => ({
+    popover: { padding: '1% 2%' },
     accountCircle: {
         width: '60%',
         height: '60%'
     }
 });
-const LoginButton = ({ classes }) => (
+const LoginButton = ({ classes, open, togglePopover }) => [
   <IconButton
+    id="login-btn"
     aria-owns={/* open ? 'menu-appbar' : */ null}
     aria-haspopup="true"
-    onClick={() => {}}
+    onClick={togglePopover}
     color="contrast"
   >
     <AccountCircle className={classes.accountCircle} />
-  </IconButton>
-);
+  </IconButton>,
+  <Popover
+    open={open}
+    key="asap-popover"
+    anchorEl={document.getElementById('login-btn')}
+    anchorReference="anchorEl"
+    anchorPosition={{ top: 200, left: 400 }}
+    onRequestClose={togglePopover}
+    anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left'
+        }}
+    transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center'
+        }}
+    classes={{ paper: classes.popover }}
+  >
+    <div>test</div>
+  </Popover>
+];
 
 LoginButton.propTypes = {
-    classes: PropTypes.object.isRequired
+    classes: PropTypes.object.isRequired,
+    open: PropTypes.bool.isRequired,
+    togglePopover: PropTypes.bool.isRequired
 };
 
-export default withStyles(styles)(LoginButton);
+const recomposeEnchancer = withStateHandlers(({ open = false }) => ({ open }), {
+    togglePopover: ({ open }) => () => ({ open: !open })
+});
+
+export default compose(recomposeEnchancer, withStyles(styles))(LoginButton);
