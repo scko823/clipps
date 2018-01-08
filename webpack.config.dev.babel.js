3ef35682e460f53a4f19ce31257dd1c1aba1c119
Sin Cheung Ko <scko823@gmail.com>
2018-01-07 21:01:59 -0700

    add webpack dev server logic

--- webpack.config.dev.babel.js
+++ webpack.config.dev.babel.js
@@ -4,33 +4,33 @@ import merge from 'webpack-merge';
 import common from './webpack.config.common.babel';
 
 const devConfig = merge(common, {
-	plugins: [new webpack.HotModuleReplacementPlugin()],
-	devServer: {
-		contentBase: path.join(__dirname, 'dist'),
-		staticOptions: {
-			redirect: true,
-		},
-		compress: true,
-		hot: true,
-		https: true,
-		port: 9000,
-		historyApiFallback: {
-			index: 'index.html',
-			rewrites: [
-				{
-					from: /^\/boards\/|\/add$/,
-					to: '/index.html',
-				},
-				{
-					from: /\.bundle.js$/,
-					to(context) {
-						return `${context.parsedUrl.pathname}`;
-					},
-				},
-			],
-		},
-	},
-	devtool: 'cheap-module-source-map',
+    plugins: [new webpack.HotModuleReplacementPlugin()],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        staticOptions: {
+            redirect: true
+        },
+        compress: true,
+        hot: true,
+        https: true,
+        port: 9000,
+        historyApiFallback: {
+            index: 'index.html',
+            rewrites: [
+                {
+                    from: /\.bundle.js$/,
+                    to(context) {
+                        return `${context.parsedUrl.pathname}`;
+                    }
+                },
+                {
+                    from: /./,
+                    to: '/index.html'
+                }
+            ]
+        }
+    },
+    devtool: 'cheap-module-source-map'
 });
 
 export default devConfig;
