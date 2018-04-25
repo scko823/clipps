b8df5cb3166a1188ce019eaf62949d787c31a23e
Sin Cheung Ko <scko823@gmail.com>
2018-04-24 21:37:40 -0700

    add log out

--- src/components/AppBar/UserAvatar.jsx
+++ src/components/AppBar/UserAvatar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 import { withStyles } from 'material-ui/styles';
@@ -8,27 +8,31 @@ import deepOrange from 'material-ui/colors/deepOrange';
 import red from 'material-ui/colors/red';
 import teal from 'material-ui/colors/teal';
 import brown from 'material-ui/colors/brown';
-import { compose } from 'recompose';
+import Popover from 'material-ui/Popover';
+import Paper from 'material-ui/Paper';
+import List, { ListItem, ListItemText } from 'material-ui/List';
+
+import { compose, withStateHandlers } from 'recompose';
 
 const styles = theme => ({
 	'0': {
-		backgroundColor: deepOrange[600],
-		margin: `0 ${theme.spacing.unit * 1.5}px`
+		backgroundColor: deepOrange[600]
 	},
 	'1': {
-		backgroundColor: red[900],
-		margin: `0 ${theme.spacing.unit * 1.5}px`
+		backgroundColor: red[900]
 	},
 	'2': {
-		backgroundColor: teal[800],
-		margin: `0 ${theme.spacing.unit * 1.5}px`
+		backgroundColor: teal[800]
 	},
 	'3': {
-		backgroundColor: brown[800],
+		backgroundColor: brown[800]
+	},
+	fakeLink: {
+		cursor: 'pointer',
 		margin: `0 ${theme.spacing.unit * 1.5}px`
 	}
 });
-const UserAvatar = ({ classes }) => {
+const UserAvatar = ({ classes, popoverOpen, togglePopover, onLogOut }) => {
 	const firstInit = localStorage.getItem('firstName')[0].toUpperCase();
 	const lastInit = localStorage.getItem('lastName')[0].toUpperCase();
 	const initials = `${firstInit}${lastInit}`;
@@ -38,13 +42,59 @@ const UserAvatar = ({ classes }) => {
 			.split('')
 			.map(c => c.charCodeAt(0))
 			.reduce((x, y) => x + y) % 4;
-	return <Avatar className={classes[hash]}>{initials}</Avatar>;
+	return (
+  <Fragment>
+    <Avatar
+      classes={{ root: classes.fakeLink }}
+      onClick={togglePopover}
+      id="user-avatar"
+      className={classes[hash]}
+    >
+      {initials}
+    </Avatar>
+    <Popover
+      open={popoverOpen}
+      anchorEl={document.getElementById('user-avatar')}
+      anchorReference="anchorEl"
+      anchorPosition={{ top: 200, left: 400 }}
+      transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+      anchorOrigin={{
+					vertical: 'bottom',
+					horizontal: 'left'
+				}}
+      onClose={togglePopover}
+    >
+      <Paper>
+        <List>
+          <ListItem button component="a" onClick={onLogOut}>
+            <ListItemText primary="Logout" />
+          </ListItem>
+        </List>
+      </Paper>
+    </Popover>
+  </Fragment>
+	);
 };
 
 UserAvatar.propTypes = {
-	classes: PropTypes.object.isRequired
+	classes: PropTypes.object.isRequired,
+	popoverOpen: PropTypes.bool.isRequired,
+	togglePopover: PropTypes.func.isRequired,
+	onLogOut: PropTypes.func.isRequired
 };
 
-// const recomposeEnchancer = withProps(({ history }) => ({ login: () => history.push('/login') }));
+const recomposeEnchancer = withStateHandlers(
+	({ popoverOpen = false }) => ({
+		popoverOpen
+	}),
+	{
+		togglePopover: ({ popoverOpen }) => () => ({ popoverOpen: !popoverOpen }),
+		onLogOut: (_, { history, onLogout: notifyLogOut }) => () => {
+			localStorage.clear();
+			notifyLogOut();
+			history.push('/');
+		}
+	}
+);
 
-export default compose(withRouter, withStyles(styles))(UserAvatar);
+export default compose(withRouter, withStyles(styles), recomposeEnchancer)(UserAvatar);
